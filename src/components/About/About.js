0e794376e1a2376cef278ba59fbcf296d1899904
ShivpujanKr-NITJsr <sahil.8228053857@gmail.com>
2024-05-09 20:42:31 +0530

    updating code

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,7 +13,7 @@ const About = () => {
               About <span className="purple"> MYSELF </span> 
             </h1>
             <p className="home-about-body">
-              I love to explore new technologies which fills me with excitement and ignites my passion for discovering the latest advancement.
+              I love to explore new technologies, which fills me with excitement and ignites my passion for discovering the latest advancement.
               <br />
               <br />Embark on a transformative journey with me, your dedicated 
               <i>
